f692898a6e8299307c0e563d9bd2eb4847815b52
Adwait S Purao <adwait.purao@spit.ac.in>
2023-08-15 17:02:13 +0530

    Add files via upload

---
+++ Section 013. Working With REST APIs - SpaceX Project/code/server/src/routes/launches/launches.controller.js
@@ -0,0 +1,71 @@
+const {
+  getAllLaunches,
+  scheduleNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
+
+const { getPagination } = require("../../services/query");
+
+async function httpGetAllLaunches(req, res) {
+  const { skip, limit } = getPagination(req.query);
+  const launches = await getAllLaunches(skip, limit);
+  return res.status(200).json(launches);
+}
+
+async function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  await scheduleNewLaunch(launch);
+  console.log(launch);
+  return res.status(201).json(launch);
+}
+
+async function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  //If launch doesn't exist
+  const existsLaunch = existsLaunchWithId(launchId);
+  if (!existsLaunch) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  // If launch does exist
+  const aborted = await abortLaunchById(launchId);
+  if (!aborted) {
+    return res.status(400).json({
+      error: "Launch not aborted",
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+  });
+}
+
+module.exports = {
+  httpAddNewLaunch,
+  httpGetAllLaunches,
+  httpAbortLaunch,
+};
